2aadc8a27e39e03d530cc8125ade79dc3e0abd14
Yusuf Felekoğlu <yusuf.felekoglu@ogr.sakarya.edu.tr>
2020-12-24 18:20:26 +0300

    Entity framework paketleri eklendi ve paketlerin surumunden kaynaklanan sorun giderildi.

--- webProje/Startup.cs
+++ webProje/Startup.cs
@@ -42,7 +42,6 @@ namespace webProje
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
             }
             else
             {
